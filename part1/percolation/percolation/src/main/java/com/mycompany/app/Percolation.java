480aba6ed80c9317d06f8f0d619a5aeb43f17486
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-26 20:02:22 +0900

    [percolation] Ready to do homework.

--- part1/percolation/percolation/src/main/java/com/mycompany/app/Percolation.java
+++ part1/percolation/percolation/src/main/java/com/mycompany/app/Percolation.java
@@ -8,14 +8,19 @@ public class Percolation {
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
-
+        // Throw an IllegalArgumentException in the constructor if n ≤ 0. 
     }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {}
+    public void open(int row, int col) {
+        // Throw an IllegalArgumentException if any argument to open(), isOpen(), or isFull() is outside its prescribed range.
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col) { return true; }
+    public boolean isOpen(int row, int col) { 
+        // Throw an IllegalArgumentException if any argument to open(), isOpen(), or isFull() is outside its prescribed range.
+        return true; 
+    }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) { return true;}
