5efe40af1c02e77f4b57a7d1433f605917a1fb00
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-02-26 19:45:40 +0900

    WIP

--- part1/percolation/percolation/src/main/java/com/mycompany/app/Percolation.java
+++ part1/percolation/percolation/src/main/java/com/mycompany/app/Percolation.java
@@ -1,3 +1,5 @@
+package com.mycompany.app;
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
@@ -5,23 +7,25 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 public class Percolation {
 
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n) {
+
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col) {}
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col)
+    public boolean isOpen(int row, int col) { return true; }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col) { return true;}
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites() { return 1;}
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates() { return true; }
 
     // test client (optional)
-    public static void main(String[] args)
+    public static void main(String[] args) {}
 }
