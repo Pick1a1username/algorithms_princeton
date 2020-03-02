eb2c835695855cda25ccc902ee27e193eace3664
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-03-03 08:00:53 +0900

    [percolation] PercolationStats can be instantiated.

--- part1/percolation/percolation/src/main/java/com/mycompany/app/PercolationStats.java
+++ part1/percolation/percolation/src/main/java/com/mycompany/app/PercolationStats.java
@@ -1,8 +1,23 @@
+package com.mycompany.app;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
+
+    private int size;
+    private int trials;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         // Throw an IllegalArgumentException in the constructor if either n ≤ 0 or trials ≤ 0. 
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("The arguments should be greater than zero.");
+        }
+
+        this.size = n;
+        this.trials = trials;
     }
 
     // sample mean of percolation threshold
---
+++ part1/percolation/percolation/src/test/java/com/mycompany/app/PercolationStatsTest.java
@@ -0,0 +1,65 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class PercolationStatsTest 
+{
+    @Test
+    public void instantiateClass()
+    {
+        PercolationStats newObject = new PercolationStats(3, 10);
+        assertNotNull(newObject);
+    }
+
+    @Test
+    public void instantiateClassException()
+    {
+        try {
+            PercolationStats newObject = new PercolationStats(-3, -10);
+            fail("Exception not thrown");
+        } catch (IllegalArgumentException e) {
+            assertEquals("The arguments should be greater than zero.", e.getMessage());
+        } catch (Exception e) {
+            fail("Unexpected exception thrown");
+        }
+    }
+
+    // @Test
+    // public void testIsOpen()
+    // {
+    //     Percolation grid = new Percolation(3);
+    //     assertEquals(false, grid.isOpen(1, 1));
+    //     grid.open(1, 1);
+    //     assertEquals(true, grid.isOpen(1, 1));
+
+    // }
+
+    // @Test
+    // public void testIsOpenException()
+    // {
+    //     try {
+    //         Percolation gridOne = new Percolation(3);
+    //         gridOne.isOpen(4,1);
+    //         fail("Exception not thrown");
+    //     } catch (IllegalArgumentException e) {
+    //         assertEquals("Out of Range", e.getMessage());
+    //     } catch (Exception e) {
+    //         fail("Unexpected exception thrown");
+    //     }
+
+    //     try {
+    //         Percolation gridTwo = new Percolation(3);
+    //         gridTwo.isOpen(1,4);
+    //     } catch (IllegalArgumentException e) {
+    //         assertEquals("Out of Range", e.getMessage());
+    //     } catch (Exception e) {
+    //         fail("Unexpected exception thrown");
+    //     }
+    // }
+
+}
